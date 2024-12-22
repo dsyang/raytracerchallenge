5975c3c14b6adc62fca132ddbbb2708fda7e1001
= <=>
2024-12-21 23:26:59 -0800

    graphics!

--- composeApp/src/commonMain/kotlin/com/dsyang92/openrndr/OpenRndrMain.kt
+++ composeApp/src/commonMain/kotlin/com/dsyang92/openrndr/OpenRndrMain.kt
@@ -7,6 +7,10 @@ import com.dsyang92.raytracerchallenge.norm
 import com.dsyang92.raytracerchallenge.plus
 import com.dsyang92.raytracerchallenge.point
 import com.dsyang92.raytracerchallenge.vector
+import com.dsyang92.raytracerchallenge.times
+import org.openrndr.math.Vector2
+import org.openrndr.shape.Circle
+import org.openrndr.shape.LineSegment
 
 
 data class Projectile(
@@ -24,41 +28,83 @@ fun tick(p: Projectile, e: Environment): Projectile {
     val newV = p.velocity + e.gravity + e.wind
     return Projectile(newPos, newV)
 }
-
+const val WIDTH = 800
+const val HEIGHT= 600
 fun main() = application {
     configure {
-        width = 800
-        height = 600
+        width = WIDTH
+        height = HEIGHT
     }
     program {
 
-        var projectile: Projectile? = null
+        val scale = 15.0
+        val origin = Tuple.p(WIDTH/2.0, HEIGHT/2.0, 0.0)
+
+        val projectile: MutableList<Projectile> = mutableListOf()
         val environment: Environment = Environment(
-            gravity = Tuple.v(0.0, -0.1, 0.0),
-            wind = Tuple.v(-0.01, 0.0, 0.0)
+            gravity = Tuple.v(0.0, -0.1, 0.0) * scale,
+            wind = Tuple.v(-0.0, 0.0, 0.0) * scale
         )
 
         keyboard.keyUp.listen {
             if (it.name == "t") {
-                val p = projectile
+                val p = projectile.lastOrNull()
                 p?.let {
-                    projectile = tick(it, environment)
+                    projectile.add(tick(it, environment))
                 }
                 println("Tick on $p")
+                println("Next p = ${projectile.lastOrNull()}")
+            } else if (it.name == "r") {
+                projectile.clear()
+                projectile.add(Projectile(
+                    point(0, 1, 0),
+                    vector(1, 1, 0) * scale
+                ))
+
+                println("Projectile created: $projectile")
             }
-            println("Projectile: $projectile")
         }
 
         mouse.buttonUp.listen {
-            projectile = Projectile(
+            projectile.clear()
+            projectile.add(Projectile(
                 point(0, 1, 0),
-                vector(1, 1, 0).norm()
-            )
+                vector(1, 1, 0) * scale
+            ))
             println("Projectile created: $projectile")
         }
 
         extend {
             drawer.clear(ColorRGBa.PINK)
+
+            //draw axes
+            drawer.fill = ColorRGBa.BLACK
+            drawer.lineSegments(listOf(
+                LineSegment(WIDTH/2.0, 0.0, WIDTH/2.0, HEIGHT.toDouble()),
+                LineSegment(0.0, HEIGHT/2.0, WIDTH.toDouble(), HEIGHT/2.0)
+            ))
+            drawer.circle(Circle(origin.x, origin.y, 3.0))
+            drawer.fill = null
+
+            // draw cannon
+            projectile.forEachIndexed { i, p ->
+                if (i < projectile.size - 1) {
+                    drawer.fill = ColorRGBa.GRAY
+                } else {
+                    drawer.fill = ColorRGBa.CYAN
+                }
+                drawer.circle(Circle(translate(p, origin), 4.0))
+            }
+
         }
     }
 }
+
+private fun translate(p: Projectile, origin: Point): Vector2 {
+    return p.position.let { pos ->
+        val newX = pos.x + origin.x
+        val newY = (-pos.y) + origin.y
+        Vector2(newX, newY)
+    }
+}
+
